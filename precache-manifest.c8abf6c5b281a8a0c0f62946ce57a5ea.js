946fa6e319fa67bdcf074f6948dd4edbaa10e85b
KESHAV GUPTA <iamkeshavgupta27@gmail.com>
2021-06-08 21:03:08 +0530

    first commit

---
+++ precache-manifest.c8abf6c5b281a8a0c0f62946ce57a5ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b61e4cd767f1ff54ca07ad06584b2a43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2bca442b46bde2ec9ec",
+    "url": "/static/css/main.992dc630.chunk.css"
+  },
+  {
+    "revision": "1a9702781ee7623e7b3f",
+    "url": "/static/js/2.104f0a7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.104f0a7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2bca442b46bde2ec9ec",
+    "url": "/static/js/main.fa488d6e.chunk.js"
+  },
+  {
+    "revision": "1877e69cc92e77fffa19",
+    "url": "/static/js/runtime-main.147a4b9c.js"
+  }
+]);
